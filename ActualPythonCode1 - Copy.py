2f524c063ce366f459f7140f4ea3174b7fdf0691
Andrew Ladrido <91585809+ALadrido@users.noreply.github.com>
2022-04-19 19:23:25 -0400

    Add files via upload

---
+++ ActualPythonCode1 - Copy.py
@@ -0,0 +1,22 @@
+print("Hello! My name is Andrew and I have 2 pairs of socks on.")
+
+print("Here is a cool picture of a bird with a birthday hat on.")
+print("   ii")
+print("   (o>")
+print("\\_//)")
+print(" \_/_)")
+print("  _|_")
+
+print("I was born and raised in Penn State!")
+print("__________  _____________ ___")
+print("\______   \/   _____/    |   \\")
+print(" |     ___/\_____  \|    |   /")
+print(" |    |    /        \    |  / ")
+print(" |____|   /_______  /______/  ")
+print("                  \/         ")
+
+yearAge = input("Please enter your age: ")
+yearAge = int(yearAge)
+yearExp = 72.6 - yearAge
+print("You have " + str(yearExp) + " more years before you hit the average life expectancy!")
+
